0bd9dd3e0fa2e4c334d24de3dedcfe75016fc483
Ramon <ramon.wenger@edorex.ch>
2016-10-03 14:56:23 +0200

    Update readme

--- app.js
+++ app.js
@@ -1,82 +1,79 @@
 'use strict';
 
 var express = require('express'),
-	fs = require('fs'),
-	sqlite =  require('sqlite3').verbose(),
-	bodyParser = require('body-parser');
+  fs = require('fs'),
+  sqlite = require('sqlite3').verbose(),
+  bodyParser = require('body-parser');
 
 var config = require('./config.json');
 
 var app = express();
-	
+
 var file = 'address.db';
 var exists = fs.existsSync(file);
 
-
 var db = new sqlite.Database(file);
 
-db.serialize(function(){
-	if(!exists){
-		db.run("CREATE TABLE Address (url TEXT)");
+db.serialize(function() {
+  if (!exists) {
+    db.run("CREATE TABLE Address (url TEXT)");
 
-		var statement = db.prepare("INSERT into Address VALUES ('')");
-		statement.run();
-		statement.finalize();
-	}
+    var statement = db.prepare("INSERT into Address VALUES ('')");
+    statement.run();
+    statement.finalize();
+  }
 });
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response){
-	getAddress(function(address){
-		var redirect =  address.url;
-		response.redirect(redirect);
-	});
-})
-.get('/config',function(request, response){
-	getAddress(function(address){
-		response.render('index.jade', address);
-	});
-})
-.post('/config', function(request, response){
-	var url = request.body.url;
-	// var port = request.body.port;
-	if(url >''){
-		db.serialize(function(){
-			db.exec("DELETE FROM Address; INSERT INTO Address (url) VALUES ('" + url + "');", function(err){
-				if(err){
-					console.log(err);
-				}
-				getAddress(function(address){	
-					response.render('index.jade', address);
-				});
-			});
-		});
-	}else{
+app.get('/', function(request, response) {
+    getAddress(function(address) {
+      var redirect = address.url;
+      response.redirect(redirect);
+    });
+  })
+  .get('/config', function(request, response) {
+    getAddress(function(address) {
+      response.render('index.jade', address);
+    });
+  })
+  .post('/config', function(request, response) {
+    var url = request.body.url;
+    if (url > '') {
+      db.serialize(function() {
+        db.exec("DELETE FROM Address; INSERT INTO Address (url) VALUES ('" + url + "');", function(err) {
+          if (err) {
+            console.log(err);
+          }
+          getAddress(function(address) {
+            response.render('index.jade', address);
+          });
+        });
+      });
+    } else {
 
-		getAddress(function(address){	
-			response.render('index.jade', address);
-		});
-	}
-})
-.listen(config.port);
+      getAddress(function(address) {
+        response.render('index.jade', address);
+      });
+    }
+  })
+  .listen(config.port);
 
-function getAddress(callback){
-	db.serialize(function(){
-		db.get('SELECT url from Address', function(err, address){
-			if(err){
-				return;
-			}
-			callback(address);
-		});
-	});
+function getAddress(callback) {
+  db.serialize(function() {
+    db.get('SELECT url from Address', function(err, address) {
+      if (err) {
+        return;
+      }
+      callback(address);
+    });
+  });
 }
 
-console.log('Server running at http://127.0.0.1:'+config.port);
+console.log('Server running at http://127.0.0.1:' + config.port);
--- readme.md
+++ readme.md
@@ -1,10 +1,37 @@
-
+# Ghettoproxy
 
 ## Description
 
-The ghettoproxy allows you to configure a small nodejs server to point to a specified URL.
-You can then favorite the ghettoproxy's URL on your test devices, without always having to type in your test URL.
+The ghettoproxy is a small local server that redirects every request to 
+a configured URL. 
+
+This is very useful i.e. if you want to test a website (with a cryptic 
+URL, like one currently in development) on multiple mobile devices. 
+
+With ghettoproxy you can just favorite ghettoproxy's URL on the mobile
+ devices, and just re-configure the URL ghettoproxy points to, instead 
+ of re-entering the URL on the mobile devices every time.  
  
-## Installation
+## Installation 
+For ghettoproxy to run, you need to have a current installation of NodeJS.  (See the [official page](https://nodejs.org/en/download/) for instructions)
+
+ ``` bash
+ git clone https://github.com/edorex/ghettoproxy
+ cd ghettoproxy
+ npm install
+ node app.js 
+ ```
+
+
+
+## Usage
+
+Point your browser to http://localhost:8081/config to configure where the proxy should point. 
+
+Once an URL has been set, http://localhost:8081/ will point to that URL.   
  
+ ## Config
+ 
+ If you want to change the port ghettoproxy runs on, you can change 
+ it in config.json
  
